ad33291821940a599a8874c391720250aee6f591
Nuno Silva <little.coding.fox@gmail.com>
2017-05-06 15:57:10 +0100

    Refactored code into StaticWikiCore library;

--- StaticWiki/Program.cs
+++ StaticWiki/Program.cs
@@ -4,24 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using Markdig;
 
 namespace StaticWiki
 {
-    class FileInfo
-    {
-        public string baseName, text;
-    };
-
     class Program
     {
-        private const string NavigationFileName = "Navigation";
-
-        private static string MarkdownStrippedString(string markdownString, MarkdownPipeline pipeline)
-        {
-            return Markdown.ToHtml(markdownString.Replace("_", " "), pipeline).Replace("<p>", "").Replace("</p>", "");
-        }
-
         static void Main(string[] args)
         {
             if (args.Length == 0)
@@ -72,228 +59,17 @@ namespace StaticWiki
                 }
             }
 
-            var pipeline = new MarkdownPipelineBuilder().UsePipeTables().UseBootstrap().Build();
-
             Console.WriteLine("StaticWiki starting up with values:");
             Console.WriteLine(string.Format("From Directory: \"{0}\"", fromDirectory));
             Console.WriteLine(string.Format("To Directory: \"{0}\"", toDirectory));
             Console.WriteLine(string.Format("Theme File: \"{0}\"", themeFileName));
             Console.WriteLine(string.Format("Base Page Title: \"{0}\"", basePageTitle));
 
-            string[] files = new string[0];
+            string logMessage = "";
 
-            try
-            {
-                files = Directory.GetFiles(fromDirectory, "*.txt");
-            }
-            catch(Exception)
-            {
-                Console.WriteLine(string.Format("StaticWiki failed to find files at the directory '{0}'", fromDirectory));
-            }
+            StaticWikiCore.ProcessDirectory(fromDirectory, toDirectory, themeFileName, basePageTitle, ref logMessage);
 
-            var themeText = "";
-
-            try
-            {
-                StreamReader In = new StreamReader(themeFileName);
-
-                themeText = In.ReadToEnd();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(string.Format("Failed to read theme file \"{0}\": {1}", themeFileName, e.Message));
-
-                return;
-            }
-
-            var pageExtension = Path.GetExtension(themeFileName);
-
-            try
-            {
-                if(!Directory.Exists(toDirectory))
-                {
-                    Directory.CreateDirectory(toDirectory);
-                }
-            }
-            catch(Exception)
-            {
-            }
-
-            Console.WriteLine(string.Format("Processing {0} files", files.Length));
-
-            var navigationInfo = new List<KeyValuePair<string, string> >();
-
-            for (int i = 0; i < files.Length; i++)
-            {
-                var baseName = files[i].Substring(fromDirectory.Length + 1);
-                baseName = baseName.Substring(0, baseName.LastIndexOf("."));
-
-                var outName = toDirectory + "/" + baseName + pageExtension;
-
-                Console.WriteLine("... " + files[i] + "(as " + outName + ")");
-
-                try
-                {
-                    var inReader = new StreamReader(files[i]);
-                    var content = inReader.ReadToEnd();
-
-                    inReader.Close();
-
-                    if (baseName.Equals(NavigationFileName, StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        var lines = content.Split("\n".ToCharArray());
-
-                        foreach(var line in lines)
-                        {
-                            if(line.Length == 0)
-                            {
-                                continue;
-                            }
-
-                            var pieces = line.Split("=".ToCharArray());
-
-                            if(pieces.Length != 2)
-                            {
-                                Console.WriteLine("[Navigation] Invalid line '{0}': Expecting format 'Name=Link'", line);
-
-                                continue;
-                            }
-
-                            navigationInfo.Add(new KeyValuePair<string, string>(pieces[0].Replace("\n", "").Replace("\r", "").Trim(),
-                                pieces[1].Replace("\n", "").Replace("\r", "").Trim()));
-                        }
-                    }
-                    else
-                    {
-                        var fileInfo = new FileInfo();
-                        fileInfo.baseName = baseName;
-                        fileInfo.text = content;
-
-                        fileCache.Add(baseName, fileInfo);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(string.Format("Failed to process file '{0}': {1}", files[i], e.Message));
-
-                    continue;
-                }
-            }
-
-            var searchNames = new StringBuilder();
-            var searchURLs = new StringBuilder();
-
-            foreach (var pair in fileCache)
-            {
-                searchNames.Append((searchNames.Length > 0 ? ", " : "") + "\"" + MarkdownStrippedString(pair.Key, pipeline).Replace("\n", "") + "\"");
-                searchURLs.Append((searchURLs.Length > 0 ? ", " : "") + "\"" + pair.Value.baseName + "\"");
-            }
-
-            for (int i = 0; i < files.Length; i++)
-            {
-                var baseName = files[i].Substring(fromDirectory.Length + 1);
-                baseName = baseName.Substring(0, baseName.LastIndexOf("."));
-
-                var outName = Path.GetFullPath(toDirectory + "/" + baseName + pageExtension);
-
-                if (baseName.Equals(NavigationFileName, StringComparison.InvariantCultureIgnoreCase) || !fileCache.ContainsKey(baseName))
-                    continue;
-
-                var fileInfo = fileCache[baseName];
-                var outText = Markdown.ToHtml(fileInfo.text, pipeline);
-
-                var index = 0;
-                var beginNavIndex = -1;
-                var endNavIndex = -1;
-
-                var processedTitle = Markdown.ToHtml(basePageTitle + ": " + baseName.Replace("_", " "), pipeline).Replace("<p>", "").Replace("</p>", "");
-
-                try
-                {
-                    var outWriter = new StreamWriter(outName);
-
-                    var finalText = (string)themeText.Clone();
-
-                    index = finalText.IndexOf("{TITLE}");
-
-                    while (index != -1)
-                    {
-                        finalText = finalText.Substring(0, index) +  processedTitle + finalText.Substring(index + "{TITLE}".Length);
-
-                        index = finalText.IndexOf("{TITLE}");
-                    };
-
-                    index = finalText.IndexOf("{SEARCHNAMES}");
-
-                    while (index != -1)
-                    {
-                        finalText = finalText.Substring(0, index) + searchNames + finalText.Substring(index + "{SEARCHNAMES}".Length);
-
-                        index = finalText.IndexOf("{SEARCHNAMES}");
-                    };
-
-                    index = finalText.IndexOf("{SEARCHURLS}");
-
-                    while (index != -1)
-                    {
-                        finalText = finalText.Substring(0, index) + searchURLs + finalText.Substring(index + "{SEARCHURLS}".Length);
-
-                        index = finalText.IndexOf("{SEARCHURLS}");
-                    };
-
-                    index = finalText.IndexOf("{BEGINNAV}");
-
-                    if (index != -1)
-                    {
-                        finalText = finalText.Substring(0, index) + finalText.Substring(index + "{BEGINNAV}".Length);
-                        beginNavIndex = index;
-                    }
-
-                    index = finalText.IndexOf("{ENDNAV}");
-
-                    if (index != -1)
-                    {
-                        finalText = finalText.Substring(0, index) + finalText.Substring(index + "{ENDNAV}".Length);
-                        endNavIndex = index;
-
-                        if(beginNavIndex != -1)
-                        {
-                            var clonedText = finalText.Substring(beginNavIndex, endNavIndex - beginNavIndex);
-                            var processedText = new StringBuilder(finalText.Substring(0, beginNavIndex));
-
-                            for (var j = 0; j < navigationInfo.Count; j++)
-                            {
-                                var navItemText = clonedText.Replace("{NAVNAME}", navigationInfo[j].Key).Replace("{NAVLINK}", navigationInfo[j].Value);
-
-                                processedText.Append(navItemText);
-                            }
-
-                            processedText.Append(finalText.Substring(endNavIndex));
-
-                            finalText = processedText.ToString();
-                        }
-                    }
-
-                    index = finalText.IndexOf("{CONTENT}");
-
-                    if (index != -1)
-                    {
-                        finalText = finalText.Substring(0, index) + outText + finalText.Substring(index + "{CONTENT}".Length);
-                    };
-
-                    outWriter.Write(finalText);
-                    outWriter.Flush();
-                    outWriter.Close();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(string.Format("Failed to process file '{0}': {1}", files[i], e.Message));
-
-                    continue;
-                }
-            }
-
-            Console.WriteLine("OK!");
+            Console.WriteLine(logMessage);
         }
     }
 }
---
+++ StaticWikiCore/StaticWikiCore.cs
@@ -0,0 +1,281 @@
+﻿using Markdig;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticWiki
+{
+    public class StaticWikiCore
+    {
+        private class FileInfo
+        {
+            public string baseName;
+            public string text;
+        }
+
+        private const string NavigationFileName = "Navigation";
+
+        private static string MarkdownStrippedString(string markdownString, MarkdownPipeline pipeline)
+        {
+            return Markdown.ToHtml(markdownString.Replace("_", " "), pipeline).Replace("<p>", "").Replace("</p>", "");
+        }
+
+        private static void HandleTextTag(ref string finalText, string processedTitle)
+        {
+            var index = finalText.IndexOf("{TITLE}");
+
+            while (index != -1)
+            {
+                finalText = finalText.Substring(0, index) + processedTitle + finalText.Substring(index + "{TITLE}".Length);
+
+                index = finalText.IndexOf("{TITLE}");
+            }
+        }
+
+        private static void HandleSearchTags(ref string finalText, string searchNames, string searchURLs)
+        {
+            var index = finalText.IndexOf("{SEARCHNAMES}");
+
+            while (index != -1)
+            {
+                finalText = finalText.Substring(0, index) + searchNames + finalText.Substring(index + "{SEARCHNAMES}".Length);
+
+                index = finalText.IndexOf("{SEARCHNAMES}");
+            }
+
+            index = finalText.IndexOf("{SEARCHURLS}");
+
+            while (index != -1)
+            {
+                finalText = finalText.Substring(0, index) + searchURLs + finalText.Substring(index + "{SEARCHURLS}".Length);
+
+                index = finalText.IndexOf("{SEARCHURLS}");
+            }
+        }
+
+        private static void HandleNavTags(ref string finalText, List<KeyValuePair<string, string>> navigationInfo)
+        {
+            var beginNavIndex = -1;
+            var endNavIndex = -1;
+
+            var index = finalText.IndexOf("{BEGINNAV}");
+
+            if (index != -1)
+            {
+                finalText = finalText.Substring(0, index) + finalText.Substring(index + "{BEGINNAV}".Length);
+                beginNavIndex = index;
+            }
+
+            index = finalText.IndexOf("{ENDNAV}");
+
+            if (index != -1)
+            {
+                finalText = finalText.Substring(0, index) + finalText.Substring(index + "{ENDNAV}".Length);
+                endNavIndex = index;
+
+                if (beginNavIndex != -1)
+                {
+                    var clonedText = finalText.Substring(beginNavIndex, endNavIndex - beginNavIndex);
+                    var processedText = new StringBuilder(finalText.Substring(0, beginNavIndex));
+
+                    for (var j = 0; j < navigationInfo.Count; j++)
+                    {
+                        var navItemText = clonedText.Replace("{NAVNAME}", navigationInfo[j].Key).Replace("{NAVLINK}", navigationInfo[j].Value);
+
+                        processedText.Append(navItemText);
+                    }
+
+                    processedText.Append(finalText.Substring(endNavIndex));
+
+                    finalText = processedText.ToString();
+                }
+            }
+        }
+
+        private static void HandleContentTag(ref string finalText, string contentText)
+        {
+            var index = finalText.IndexOf("{CONTENT}");
+
+            if (index != -1)
+            {
+                finalText = finalText.Substring(0, index) + contentText + finalText.Substring(index + "{CONTENT}".Length);
+            }
+        }
+
+        private static List<KeyValuePair<string, string>> ProcessNavigation(string content)
+        {
+            var outNavigation = new List<KeyValuePair<string, string>>();
+            var lines = content.Split("\n".ToCharArray());
+
+            foreach (var line in lines)
+            {
+                if (line.Length == 0)
+                {
+                    continue;
+                }
+
+                var pieces = line.Split("=".ToCharArray());
+
+                if (pieces.Length != 2)
+                {
+                    Console.WriteLine("[Navigation] Invalid line '{0}': Expecting format 'Name=Link'", line);
+
+                    continue;
+                }
+
+                outNavigation.Add(new KeyValuePair<string, string>(pieces[0].Replace("\n", "").Replace("\r", "").Trim(),
+                    pieces[1].Replace("\n", "").Replace("\r", "").Trim()));
+            }
+
+            return outNavigation;
+        }
+
+        public static string ProcessFile(string sourceText, string themeText, string title, List<KeyValuePair<string, string>> navigationInfo, string[] searchNames, string[] searchURLs)
+        {
+            var pipeline = new MarkdownPipelineBuilder().UsePipeTables().UseBootstrap().Build();
+            var searchNamesString = string.Join(",", searchNames.Select(x => string.Format("\"{0}\"", MarkdownStrippedString(x, pipeline).Replace("\n", ""))).ToArray());
+            var searchURLsString = string.Join(",", searchURLs.Select(x => string.Format("\"{0}\"", x)).ToArray());
+            var contentText = Markdown.ToHtml(sourceText, pipeline);
+            var processedTitle = Markdown.ToHtml(title.Replace("_", " "), pipeline).Replace("<p>", "").Replace("</p>", "");
+
+            var finalText = (string)themeText.Clone();
+
+            HandleTextTag(ref finalText, processedTitle);
+            HandleSearchTags(ref finalText, searchNamesString, searchURLsString);
+            HandleNavTags(ref finalText, navigationInfo);
+            HandleContentTag(ref finalText, contentText);
+
+            return finalText;
+        }
+
+        public static bool ProcessDirectory(string sourceDirectory, string destinationDirectory, string themeFileName, string baseTitle, ref string logMessage)
+        {
+            var fileCache = new Dictionary<string, FileInfo>();
+            var pipeline = new MarkdownPipelineBuilder().UsePipeTables().UseBootstrap().Build();
+            var files = new string[0];
+            logMessage = "";
+
+            try
+            {
+                files = Directory.GetFiles(sourceDirectory, "*.txt");
+            }
+            catch (Exception)
+            {
+                logMessage += string.Format("StaticWiki failed to find files at the directory '{0}'\n", sourceDirectory);
+
+                return false;
+            }
+
+            var themeText = "";
+
+            try
+            {
+                StreamReader In = new StreamReader(themeFileName);
+
+                themeText = In.ReadToEnd();
+            }
+            catch (Exception e)
+            {
+                logMessage += string.Format("Failed to read theme file \"{0}\": {1}\n", themeFileName, e.Message);
+
+                return false;
+            }
+
+            var pageExtension = Path.GetExtension(themeFileName);
+
+            try
+            {
+                if (!Directory.Exists(destinationDirectory))
+                {
+                    Directory.CreateDirectory(destinationDirectory);
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            var navigationInfo = new List<KeyValuePair<string, string>>();
+
+            logMessage += string.Format("Processing {0} files\n", files.Length);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                var baseName = files[i].Substring(sourceDirectory.Length + 1);
+                baseName = baseName.Substring(0, baseName.LastIndexOf("."));
+
+                var outName = destinationDirectory + "/" + baseName + pageExtension;
+
+                try
+                {
+                    var inReader = new StreamReader(files[i]);
+                    var content = inReader.ReadToEnd();
+
+                    inReader.Close();
+
+                    if (baseName.Equals(NavigationFileName, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        navigationInfo = ProcessNavigation(content);
+                    }
+                    else
+                    {
+                        var fileInfo = new FileInfo();
+                        fileInfo.baseName = baseName;
+                        fileInfo.text = content;
+
+                        fileCache.Add(baseName, fileInfo);
+                    }
+                }
+                catch (Exception e)
+                {
+                    logMessage += string.Format("Failed to process file '{0}': {1}\n", files[i], e.Message);
+
+                    continue;
+                }
+            }
+
+            var searchNames = new List<string>();
+            var searchURLs = new List<string>();
+
+            foreach (var pair in fileCache)
+            {
+                searchNames.Add(pair.Key);
+                searchURLs.Add(pair.Value.baseName);
+            }
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                var baseName = files[i].Substring(sourceDirectory.Length + 1);
+                baseName = baseName.Substring(0, baseName.LastIndexOf("."));
+
+                var outName = Path.GetFullPath(destinationDirectory + "/" + baseName + pageExtension);
+
+                if (baseName.Equals(NavigationFileName, StringComparison.InvariantCultureIgnoreCase) || !fileCache.ContainsKey(baseName))
+                    continue;
+
+                logMessage += string.Format("... {0} (as {1})\n", files[i], outName);
+
+                var fileInfo = fileCache[baseName];
+                var processedText = ProcessFile(fileInfo.text, (string)themeText.Clone(), string.Format("{0}: {1}", baseTitle, baseName), navigationInfo, searchNames.ToArray(), searchURLs.ToArray());
+
+                try
+                {
+                    var outWriter = new StreamWriter(outName);
+                    outWriter.Write(processedText);
+                    outWriter.Flush();
+                    outWriter.Close();
+                }
+                catch (Exception e)
+                {
+                    logMessage += string.Format("Failed to process file '{0}': {1}\n", files[i], e.Message);
+                }
+            }
+
+            logMessage += "Done\n";
+
+            return true;
+        }
+    }
+}
